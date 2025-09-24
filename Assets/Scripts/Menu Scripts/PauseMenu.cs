85e28d8dfdb009435cff72487233bdb32136006b
GarlicGladiator <zhoujason2003@gmail.com>
2025-09-24 02:20:20 -0400

    Audio system implemented
    menu has sounds
    sounds still need too be linked

---
+++ Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        //Debug.Log("Loading Main Menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
